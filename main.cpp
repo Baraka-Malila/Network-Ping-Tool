ec74bfb828bde68417b57153ddf13a1e9c584b91
Baraka-Malila <Bmalila87@gmail.com>
2024-12-30 14:32:11 +0300

    Added displayPingMenu function for ping-related options with color-coded menu

--- main.cpp
+++ main.cpp
@@ -30,3 +30,15 @@ void displayMenu() {
 }
 
 
+void displayPingMenu() {
+    std::cout << "\033[1;34m"; // Blue color for the menu
+    std::cout << "\n--- Ping Menu ---\n";
+    std::cout << "1. Ping by IP Address\n";
+    std::cout << "2. Ping by Domain Name\n";
+    std::cout << "3. Specify Number of Pings\n";
+    std::cout << "4. Back to Main Menu\n";
+    std::cout << "\033[0m"; // Reset color
+    std::cout << "Choose an option: ";
+}
+
+
